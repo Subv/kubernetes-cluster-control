ed05e0c10b4487c40204650bbff645576e211ad1
Subv <subv2112@gmail.com>
2018-05-30 14:39:26 -0500

    deployment list working

--- modules/core/client/controllers/deployment_list.client.controller.js
+++ modules/core/client/controllers/deployment_list.client.controller.js
@@ -5,9 +5,11 @@
         .module('core')
         .controller('DeploymentListController',DeploymentListController);
 
-        DeploymentListController.$inject = ['$scope', '$http', '$window'];
+        DeploymentListController.$inject = ['$scope', '$http', '$window', 'Authentication'];
     
-    function DeploymentListController($scope,$http , w){
+    function DeploymentListController($scope,$http , w , A){
+
+    
 
     $scope.ListDeployments = function () {
       console.log("Deployments");
@@ -15,16 +17,28 @@
     }
     $scope.ReturnTo = function() {
         console.log("Redirecting");
-        w.location = '/deployment'
+        w.location = '/deployment';
   
       }
+    $scope.user =  A.user.username,
+    $scope.Headers = [{
+        'link' : 'link',
+        'ide': 'ide',
+    }];
+
     }
 
     function ListDeploys($scope ,$http){
         $http.post('/list', $scope.list).success(function(data){
-            console.log("List post success");
-            console.log(data);
+            /*console.log("List post success");
+            console.log("data is");
+            console.log(data);*/
             $scope.list = data;
+            $scope.found = [];
+            for (var i=0;i<data.length;i++){
+                $scope.found[i] = data[i].uniqueid;
+            }
+            console.log($scope.found);
         }).error(function (error) {
       console.log("List error");
       console.log(error);
